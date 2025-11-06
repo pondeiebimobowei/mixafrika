dd70327ef8aebb02beb2739272eae99cb0c44999
Pondei Ebimobowei <ebi4jah15@gmail.com>
2025-11-06 02:12:06 +0100

    small fixes

--- apps/backend/src/funding/funding.controller.ts
+++ apps/backend/src/funding/funding.controller.ts
@@ -1,13 +1,13 @@
 
 import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common';
 import { FundingService } from './funding.service';
-import { CreateFundingApplicationDto, createFundingApplicationDto } from './dto/create-funding-application.dto';
+import { CreateFundingApplicationDto, createFundingApplicationDto } from '@shared/shared/src/validation/funding-application.dto';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { ParsedToken } from '../decorators/parsed-token.decorator';
 import { IJwtToken } from '@shared/shared/src/types/jwt';
 import { ZodPipe } from 'src/pipes/zod-pipes';
 
-@Controller('funding')
+@Controller('v1/funding')
 export class FundingController {
   constructor(private readonly fundingService: FundingService) {}
 
--- apps/backend/src/funding/funding.service.ts
+++ apps/backend/src/funding/funding.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { FundingApplication } from '../database/models/funding_application';
-import { CreateFundingApplicationDto } from './dto/create-funding-application.dto';
+import { CreateFundingApplicationDto } from '@shared/shared/src/validation/funding-application.dto';
 
 @Injectable()
 export class FundingService {
 
-  async create(
-    createFundingApplicationDto: CreateFundingApplicationDto,
-    userId: string,  
-  ) {
-    FundingApplication.create({
+  async create( createFundingApplicationDto: CreateFundingApplicationDto, userId: string) {
+    const data =  FundingApplication.create({
       ...createFundingApplicationDto,
       user_id: userId,
     });
+
+    return {
+      success: true,
+      data,
+      message: "Application Successful"
+    }
   }
 }
