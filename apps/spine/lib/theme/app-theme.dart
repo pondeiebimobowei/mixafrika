5501ca5081de4e02e275fe3515ba90e816399611
ebimopondei <ebi4jah15@gmail.com>
2026-02-17 15:24:35 +0100

    feat: Integrate forui package for comprehensive theme management, introducing new light and dark themes and button styles.

---
+++ apps/spine/lib/theme/app-theme.dart
@@ -0,0 +1,7 @@
+import 'package:spine/theme/theme-light.dart';
+import 'package:spine/theme/theme-dark.dart';
+
+class AppTheme {
+  static final light = greenLight;
+  static final dark = greenDark;
+}
