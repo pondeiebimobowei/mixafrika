e4116e4dfe70e9a905c9a5949025c4fbcc6b308e
ebimopondei <ebi4jah15@gmail.com>
2026-04-24 12:16:29 +0100

    chore: update service worker version and refresh Flutter web build artifacts

--- apps/spine/deploy/web/flutter_bootstrap.js
+++ apps/spine/deploy/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3395978935" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3106128728" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
