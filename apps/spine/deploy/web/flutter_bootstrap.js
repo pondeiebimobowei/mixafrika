a7750397b01d70c84e31dda36fecf10dfe58ac05
pondeiebimobowei <pondeiebimobowei@gmail.com>
2026-07-06 07:21:06 +0100

    feat: update API base URL to v1 and add project roadmap documentation

--- apps/spine/deploy/web/flutter_bootstrap.js
+++ apps/spine/deploy/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3543685143" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "466479784" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
