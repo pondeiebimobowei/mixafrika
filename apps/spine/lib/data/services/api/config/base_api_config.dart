95262a2d473097806d71e0b97294cd6a45d3cf09
pondeiebimobowei <pondeiebimobowei@gmail.com>
2026-07-06 06:15:43 +0100

    docs: add project roadmap and update API service base configuration

--- apps/spine/lib/data/services/api/config/base_api_config.dart
+++ apps/spine/lib/data/services/api/config/base_api_config.dart
@@ -2,12 +2,12 @@ import 'package:dio/dio.dart';
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:spine/data/shared_preference.dart';
 
-final String baseUrl =  'https://api.mixafrika.com/v1';
+// final String baseUrl =  'https://api.mixafrika.com/v1';
 // final String baseUrl =  'http://localhost:3003/v1';
-// final String baseUrl = String.fromEnvironment(
-//   'base_url',
-//   defaultValue: 'https://api.mixafrika.com/v1',
-// );
+final String baseUrl = String.fromEnvironment(
+  'base_url',
+  defaultValue: 'https://api.mixafrika.com/v1',
+);
 
 final Dio apiPrivate =
     Dio(
