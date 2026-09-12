85b2951ea5c6244aa9c498a98713ee617abf7ec0
pondeiebimobowei <pondeiebimobowei@gmail.com>
2026-09-12 12:53:37 +0100

    refactor:  fix dayjs import syntax

--- apps/backend/src/notification/notification.service.ts
+++ apps/backend/src/notification/notification.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as dayjs from 'dayjs';
+import dayjs from 'dayjs';
 import { Notification } from 'src/database/models/notification.model';
 
 @Injectable()
